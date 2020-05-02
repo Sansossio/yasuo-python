468e019de19ea5771fa651c0ed5998b45d6cc0bf
Sansossio <juliosansossio@gmail.com>
2020-05-02 20:31:52 +0200

    simply condition

--- run_examples.py
+++ run_examples.py
@@ -10,9 +10,7 @@ apikey = os.getenv("RIOT_API_KEY")
 
 for method_name in dir(examples):
   method = getattr(examples, method_name)
-  if not callable(method):
-    continue
-  if len(examples_to_run) > 0 and method_name not in examples_to_run:
+  if not callable(method) or (len(examples_to_run) > 0 and method_name not in examples_to_run):
     continue
   message = "Method \"{}\" \nresponse: {}".format(method_name, method(apikey))
   print(message + "\n\n")
