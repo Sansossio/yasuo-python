8e62528c1b0352e7dee0ec10d305e5e9ba0a9cb5
Sansossio <juliosansossio@gmail.com>
2020-05-02 03:40:55 +0200

    Regions enum

---
+++ yasuo/enum/regions.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+class Regions(Enum):
+  BRAZIL = "BR1"
+  EU_EAST = "EUN1"
+  EU_WEST = "EUW1"
+  KOREA = "KR"
+  LAT_NORTH = "LA1"
+  LAT_SOUTH = "LA2"
+  AMERICA_NORTH = "NA1"
+  OCEANIA = "OC1"
+  TURKEY = "TR1"
+  RUSSIA = "RU"
+  JAPAN = "JP1"
+  PBE = "PBE"
