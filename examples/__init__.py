bf1ed5c577445228d4e45f7a467e371d0e3ad2fb
Sansossio <juliosansossio@gmail.com>
2020-05-03 02:23:41 +0200

    Remove champion listing from examples, because this method is too slow

--- examples/__init__.py
+++ examples/__init__.py
@@ -3,5 +3,4 @@ from .summoner_by_id import summoner_by_id
 from .summoner_by_puuid import summoner_by_puuid
 from .summoner_by_account import summoner_by_account
 from .champion_free_rotation import champion_free_rotation
-from .champion_listing import champion_listing
 from .champion_details import champion_details
--- examples/champion_listing.py
+++
@@ -1,6 +0,0 @@
-from yasuo import LolApi
-from examples.examples_config import summoner_name, region
-
-def champion_listing(apikey: str):
-  lol_api = LolApi(apikey=apikey)
-  return lol_api.champion.champion_listing()
