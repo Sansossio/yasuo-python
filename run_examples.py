110fe342c622583ec220548d0f20c516b5d8fda8
Sansossio <juliosansossio@gmail.com>
2020-05-03 02:21:24 +0200

    improve example message

--- run_examples.py
+++ run_examples.py
@@ -1,6 +1,7 @@
 import examples
 import sys
 import os
+import time
 from dotenv import load_dotenv
 from get_example_list import get_example_list
 
@@ -12,6 +13,10 @@ apikey = os.getenv("RIOT_API_KEY")
 for method_name in get_example_list():
   if (len(examples_to_run) > 0 and method_name not in examples_to_run):
     continue
+  init_time = time.time()
   method = getattr(examples, method_name)
-  message = "Method \"{}\" \nresponse: {}".format(method_name, method(apikey))
+  print("Runnig method " + method_name)
+  method_response = method(apikey)
+  method_time = time.time() - init_time
+  message = "Method \"{}\" \nresponse: {} \ntime: {:0.2f} seconds".format(method_name, method_response, method_time)
   print(message + "\n\n")
