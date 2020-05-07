a347430ec16bf204576a6430484c028319714b4b
Sansossio <juliosansossio@gmail.com>
2020-05-07 16:52:21 +0200

    Spectator - import classes

---
+++ yasuo/dto/spectator/__init__.py
@@ -0,0 +1,5 @@
+from .featured_games_dto import FeaturedGamesDTO
+from .featured_game_info_dto import FeaturedGameInfoDTO
+from .participant_dto import ParticipantDTO
+from .banned_champion_dto import BannedChampionDTO
+from .observer_dto import ObserverDTO
